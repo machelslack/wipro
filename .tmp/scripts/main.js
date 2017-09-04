a2cc1d709608be3da70a7052cfa4003c74cc270e
machelslack <machel.slack@icloud.com>
2017-09-04 20:42:31 +0100

    basic app shell
    Adding the html,scss and js skeleton files for the core components of the app shell consisting of:
    
    1) Header with a title
    2) Container for forecast cards
    3) A forecast card template
    4) A loading indicator

---
+++ .tmp/scripts/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+(function () {
+  'use strict';
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBQSxDQUFDLFlBQVc7QUFDVjtBQUlELENBTEQiLCJmaWxlIjoibWFpbi5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbigpIHtcbiAgJ3VzZSBzdHJpY3QnO1xuXG4gIFxuXG59KSgpO1xuIl19
